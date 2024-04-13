e5fe42f589a7668093e3de0e022a37107926f9c5
Shalnov.Yuri.Pipr2.3Kurs <96010611+jra1251@users.noreply.github.com>
2024-04-13 23:31:17 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -12,51 +12,48 @@ using namespace std;
 
 int main()
 {
-	setlocale(LC_ALL,"ru");
-	string path = "List_Product.txt";
-	ifstream fin;
-	fin.open(path);
-	list<Product> Menu;
-	if (fin.is_open()) {
-		std::cout << "Чтение из файла..." << std::endl;
-		
-		string  w1="";
-		int price = 0;
-		char ch;
-		while (fin.get(ch)) {
-			if (ch != '\n')
-			{
-				if (ch != ' ') {
-					if (ch>='А'&&ch<='я'/*|| ch >= 'а' && ch <= 'я'*/) {
-						w1 += ch;
-					} if (ch>='0'&&ch<='9') {
-						if (price == 0) {
-							price += (int)ch-48;
-						} else {
-							price *= 10;
-							price += (int)ch-48;
-						}
-					}
-				}
-				
-			} else {
-				Menu.push_back(Product(w1, price));
-				price = 0;
-				w1 = "";
-			}
-		}
-		cout << "меню установлено" << endl;
-	}
-	else 
-	{
-		std::cout << "Не удалось открыть файл" << std::endl;
-	}
-	Automata Coffe(Menu);
-	Coffe.ON();
-	Coffe.GetMenu();
-	int vibor,coin;
-	cin >> coin;
-	Coffe.coin(coin);
+setlocale(LC_ALL,"ru");
+string path = "List_Product.txt";
+ifstream fin;
+fin.open(path);
+list<Product> Menu;
+if (fin.is_open()) {
+std::cout << "Чтение из файла..." << std::endl;
+string  w1="";
+int price = 0;
+char ch;
+while (fin.get(ch)) {
+if (ch != '\n'){
+if (ch != ' ') {
+if (ch>='А'&&ch<='я') {
+ w1 += ch;
+} if (ch>='0'&&ch<='9') {
+if (price == 0) {
+price += (int)ch-48;
+} else {
+price *= 10;
+price += (int)ch-48;
+}
+}
+}				
+} else {
+Menu.push_back(Product(w1, price));
+price = 0;
+w1 = "";
+}
+}
+cout << "меню установлено" << endl;
+}
+else 
+{
+std::cout << "Не удалось открыть файл" << std::endl;
+}
+Automata Coffe(Menu);
+Coffe.ON();
+Coffe.GetMenu();
+int vibor,coin;
+cin >> coin;
+Coffe.coin(coin);
 	
 	cin >> vibor;
 	Coffe.choice(vibor);
