e8aae38e8e8308a5c13c7c971f509ce05844aafc
Shalnov.Yuri.Pipr2.3Kurs <96010611+jra1251@users.noreply.github.com>
2024-04-14 00:07:04 +0300

    Update Product.h

--- include/Product.h
+++ include/Product.h
@@ -1,16 +1,17 @@
+// Copyright 2024 Yuri Shalnov
 #pragma once
 #include <string>
 #include <string>
 using namespace std;
 
 class Product {
-private:
-std::string product;
-int Price;
-public:
-Product(std::string pr, int price);
-~Product();
-void print();
-int get_price();	
+ private:
+    std::string product;
+    int Price;
+ public:
+    Product(std::string pr, int price);
+    ~Product();
+    void print();
+    int get_price();	
 };
 
