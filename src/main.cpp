a3a8714c1ee781555aeac7722158cdafeb514b05
Shalnov.Yuri.Pipr2.3Kurs <96010611+jra1251@users.noreply.github.com>
2024-04-14 01:00:25 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,14 @@
-﻿// Coffee.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+// Copyright 2024 Yuri Shalnov
 
 #include <iostream>
 #include<fstream>
-//#include "List.h"
 #include<locale.h>
 #include "Product.h"
 #include <list>
 #include "Automata.h"
 
 
-int main()
-{
+int main() {
 setlocale(LC_ALL,"ru");
 stdstring path = "List_Product.txt";
 std::ifstream fin;
@@ -19,23 +16,23 @@ fin.open(path);
 std::list<Product> Menu;
 if (fin.is_open()) {
 std::cout << "Чтение из файла..." << std::endl;
-std::string  w1="";
+std::string  w1 = "";
 int price = 0;
 char ch;
 while (fin.get(ch)) {
 if (ch != '\n'){
 if (ch != ' ') {
 if (ch>='А'&&ch<='я') {
- w1 += ch;
-} if (ch>='0'&&ch<='9') {
+w1 += ch;
+} if (ch >= '0'&& ch <='9') {
 if (price == 0) {
-price += (int)ch-48;
+price += static_cast<int>(ch)-48;
 } else {
 price *= 10;
-price += (int)ch-48;
+price += static_cast<int>(ch)-48;
 }
 }
-}				
+}
 } else {
 Menu.push_back(Product(w1, price));
 price = 0;
@@ -43,15 +40,13 @@ w1 = "";
 }
 }
 std::cout << "меню установлено" << std::endl;
-}
-else 
-{
+} else {
 std::cout << "Не удалось открыть файл" << std::endl;
 }
 Automata Coffe(Menu);
 Coffe.ON();
 Coffe.GetMenu();
-int vibor,coin;
+int vibor, coin;
 std::cin >> coin;
 Coffe.coin(coin);
 std::cin >> vibor;
